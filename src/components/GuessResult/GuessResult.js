c8bc5378c92162c4aac6a30a707d1a341f9915c2
Jeff Buzulencia <jeff.buzulencia@gmail.com>
2025-01-07 11:25:01 -0500

    exercise 3 first try, no components

--- src/components/GuessResult/GuessResult.js
+++ src/components/GuessResult/GuessResult.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResult({ guessList }) {
+  function getLetter(guessIndex, letterIndex) {
+    if (guessIndex < guessList.length) {
+      return guessList[guessIndex][letterIndex];
+    }
+    return undefined;
+  }
   return (
     <div className="guess-results">
-      {guessList.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
-      ))}
+      {
+        // guessList.map((guess, index) => (
+        //   <p className="guess" key={index}>
+        //     {guess}
+        //   </p>
+        // ))
+
+        range(0,NUM_OF_GUESSES_ALLOWED).map((num) => (
+          <p className="guess" key={num}>
+            <span className="cell">{getLetter(num, 0)}</span>
+            <span className="cell">{getLetter(num, 1)}</span>
+            <span className="cell">{getLetter(num, 2)}</span>
+            <span className="cell">{getLetter(num, 3)}</span>
+            <span className="cell">{getLetter(num, 4)}</span>
+          </p>
+        ))
+      }
     </div>
   );
 }
